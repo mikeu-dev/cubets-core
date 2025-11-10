03c86c6ff68c79f38a493f66f5910c875923eb30
mikeu-dev <rikiruswandi28@gmail.com>
2025-11-10 19:48:13 +0700

    this is ScoreEntry class. Model of Score module

---
+++ Modules/Score/Models/ScoreEntry.cs
@@ -0,0 +1,12 @@
+﻿namespace cubets_core.Modules.Score.Models
+{
+    public class ScoreEntry
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+
+        public int Score { get; set; }
+
+        public DateTime AchievedAt { get; set; }
+    }
+}
