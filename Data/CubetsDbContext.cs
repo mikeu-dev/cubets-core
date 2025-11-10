b374dfa7d2caae5f430f3f2368ef0131863df65c
mikeu-dev <rikiruswandi28@gmail.com>
2025-11-10 19:51:09 +0700

    this is CubetsDbContext class. DbContext MySQL

---
+++ Data/CubetsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using cubets_core.Modules.Auth.Models;
+using cubets_core.Modules.GameState.Models;
+using cubets_core.Modules.Score.Models;
+
+namespace cubets_core.Data
+{
+    public class CubetsDbContext : DbContext
+    {
+        public CubetsDbContext(DbContextOptions<CubetsDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<PlayerState> PlayerStates { get; set; }
+        public DbSet<ScoreEntry> ScoreEntries { get; set; }
+    }
+}
