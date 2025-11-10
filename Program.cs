b7cf39795b1466c1727975dd1db450bf2005167c
mikeu-dev <rikiruswandi28@gmail.com>
2025-11-10 19:11:07 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using MySqlConnector;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMySqlDataSource(builder.Configuration.GetConnectionString("ConnectionStrings")!);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
