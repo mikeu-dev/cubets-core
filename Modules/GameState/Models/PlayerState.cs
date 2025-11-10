cb9e2035b2886f2267e3703b1b9dc6cc6695dea5
mikeu-dev <rikiruswandi28@gmail.com>
2025-11-10 19:47:38 +0700

    this is PlayerState class. Model of GameState module

---
+++ Modules/GameState/Models/PlayerState.cs
@@ -0,0 +1,15 @@
+﻿namespace cubets_core.Modules.GameState.Models
+{
+    public class PlayerState
+    {
+        public int PlayerId { get; set; }
+
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public int Lives { get; set; }
+
+        public int Score { get; set; }
+    }
+}
