756940b88b6f249a836ed118c763ea623da24086
mikeu-dev <rikiruswandi28@gmail.com>
2025-11-11 15:48:57 +0700

    Score module

---
+++ Modules/Score/Controllers/ScoreController.cs
@@ -0,0 +1,30 @@
+﻿using cubets_core.Modules.Score.DTOs;
+using cubets_core.Modules.Score.Models;
+using cubets_core.Modules.Score.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cubets_core.Modules.Score.Controllers
+{
+    public class ScoreController
+    {
+        private readonly ScoreService _scoreService;
+
+        public ScoreController(ScoreService scoreService) => _scoreService = scoreService;
+
+        [HttpGet("playerId")]
+        public ActionResult<ScoreEntryDto> GetHighScore(int playerId)
+        {
+            var highScore = _scoreService.GetTopScoresAsync(playerId);
+            if (highScore == null) return NotFound();
+            return Ok(highScore);
+        }
+        private ActionResult<ScoreEntryDto> Ok(Task<List<ScoreEntry>> highScore)
+        {
+            throw new NotImplementedException();
+        }
+        private ActionResult<ScoreEntryDto> NotFound()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Modules/Score/DTOs/ScoreEntryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace cubets_core.Modules.Score.DTOs
+{
+    public class ScoreEntryDto
+    {
+        public int PlayerId { get; set; }
+        public int Score { get; set; }
+        public DateTime AchievedAt { get; set; }
+    }
+}
