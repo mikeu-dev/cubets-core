5e0c54ad42270b3bcb1bba46063421aab1321cad
mikeu-dev <rikiruswandi28@gmail.com>
2025-11-14 16:42:27 +0700

    Make ServiceCollectionExtensions class

---
+++ Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using cubets_core.Common;
+using cubets_core.Helpers;
+using cubets_core.Modules.Auth.Services;
+using cubets_core.Modules.Player.Services;
+using cubets_core.Modules.Score.Services;
+
+namespace CubetsCore.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IResponseService, ResponseService>();
+            services.AddScoped<JwtService>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<PlayerService>();
+            services.AddScoped<ScoreService>();
+
+            return services;
+        }
+    }
+}
